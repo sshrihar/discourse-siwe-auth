0f5289fc60e0a93721f3db64ef04729dbe69a0d8
Saurabh Shrihar <121215809+sshrihar@users.noreply.github.com>
2025-08-28 11:38:11 +0400

    Uncomment gem dependencies in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,21 +12,17 @@ register_asset 'stylesheets/discourse-siwe.scss'
   ../lib/omniauth/strategies/siwe.rb
 ].each { |path| load File.expand_path(path, __FILE__) }
 
-# gem 'pkg-config', '1.5.0', require: false
-# gem 'forwardable', '1.3.3', require: false
-# gem 'mkmfmf', '0.4', require: false
-# gem 'keccak', '1.3.0', require: false
-# gem 'zip', '2.0.2', require: false
-# gem 'mini_portile2', '2.8.0', require: false
-# gem 'rbsecp256k1', '6.0.0', require: false
-# gem 'konstructor', '1.0.2', require: false
-# gem 'ffi-compiler', '1.0.1', require: false
-# gem 'scrypt', '3.0.7', require: false
-# gem 'eth', '0.5.11', require: false
-# gem 'siwe', '1.1.2', require: false
-
+gem 'pkg-config', '1.5.0', require: false
+gem 'forwardable', '1.3.3', require: false
+gem 'mkmfmf', '0.4', require: false
 gem 'keccak', '1.3.0', require: false
+gem 'zip', '2.0.2', require: false
+gem 'ffi', '1.17.2', require: false
+gem 'mini_portile2', '2.8.0', require: false
 gem 'rbsecp256k1', '6.0.0', require: false
+gem 'konstructor', '1.0.2', require: false
+gem 'ffi-compiler', '1.0.1', require: false
+gem 'scrypt', '3.0.7', require: false
 gem 'eth', '0.5.11', require: false
 gem 'siwe', '1.1.2', require: false
 
